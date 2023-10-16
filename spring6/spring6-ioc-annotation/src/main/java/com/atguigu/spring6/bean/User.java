73bae2cdd0c59d35bc6416cb5c76b6d5779d935c
uprightclear <uprightclear@gmail.com>
2023-10-16 14:40:10 -0700

    Bean Management based on annotation

---
+++ spring6/spring6-ioc-annotation/src/main/java/com/atguigu/spring6/bean/User.java
@@ -0,0 +1,10 @@
+package com.atguigu.spring6.bean;
+
+import org.springframework.stereotype.Controller;
+
+//@Component(value = "user") //<bean id="user" class="...">
+//@Repository
+//@Service
+@Controller
+public class User {
+}

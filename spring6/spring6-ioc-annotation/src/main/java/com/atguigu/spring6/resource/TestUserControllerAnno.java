acfe56eba67dd7628a8d8c7b5e1dd8604c6c6d5e
uprightclear <uprightclear@gmail.com>
2023-10-17 22:39:37 -0700

    Bean Management based on annotation 2

---
+++ spring6/spring6-ioc-annotation/src/main/java/com/atguigu/spring6/resource/TestUserControllerAnno.java
@@ -0,0 +1,17 @@
+package com.atguigu.spring6.resource;
+
+import com.atguigu.spring6.config.SpringConfig;
+import com.atguigu.spring6.resource.controller.UserController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class TestUserControllerAnno {
+
+    public static void main(String[] args) {
+        //加载配置类
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(SpringConfig.class);
+        UserController controller = context.getBean(UserController.class);
+        controller.add();
+    }
+}
